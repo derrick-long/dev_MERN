186cde8c542714bc4412984d9ff8b223a949e060
derrick-long <long.derrick8@gmail.com>
2019-03-06 22:38:17 -0500

    add User model

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
+//route GET api/posts/test
+// desc tests post route
+// access public
+
 router.get('/test', (req,res)=>
     res.json({msg: "howdy partner posts works"}));
 
