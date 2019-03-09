6520e94c823940341dfede0b50fa8b37ecb39195
derrick-long <long.derrick8@gmail.com>
2019-03-08 20:10:58 -0500

    implement json token for login

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI: 'mongodb://Derrick:Derrick246@ds153495.mlab.com:53495/dev_mern'
+    mongoURI: 'mongodb://Derrick:Derrick246@ds153495.mlab.com:53495/dev_mern',
+    secretOrKey: 'stuff'
 }
