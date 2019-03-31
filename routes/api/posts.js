0c52317b2037886786f02cccf8b0414f40379953
derrick-long <long.derrick8@gmail.com>
2019-03-30 20:00:29 -0400

    add post route and validation

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const passport = require('passport');
+const Post = require('../../models/Posts');
+//validation 
+const  validatePostInput = require('../../validation/post');
 
 //route GET api/posts/test
 // desc tests post route
@@ -7,5 +12,29 @@ const router = express.Router();
 
 router.get('/test', (req,res)=>
     res.json({msg: "howdy partner posts works"}));
+
+//route POST api/posts/
+// desc create post
+// access private
+
+router.post('/',passport.authenticate('jwt', { session: false}), (req,res)=> {
+    const {errors, isValid } = validatePostInput(req.body);
+
+    if(!isValid){
+        //if errors send 400 with error obj
+        return res.status(400).json(errors);
+    }
+
+
+    const newPost =  new Post ({
+        text: req.body.text,
+        name: req.body.name,
+        avatar: req.body.avatar,
+        user: req.user.id
+
+    });
+
+     newPost.save().then(post => res.json(post));
+});
 
 module.exports = router;
