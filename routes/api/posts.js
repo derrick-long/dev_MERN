aba327b3ed2bbae7b18af1c05e2c5b7f18edc678
derrick-long <long.derrick8@gmail.com>
2019-04-11 12:00:21 -0400

    add unlike post route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -88,7 +88,7 @@ router.delete('/:id',passport.authenticate('jwt', {session:false}), (req,res) =>
 //route POST api/posts/like/:id
 // desc Like post
 // access private
-router.post('like/:id',passport.authenticate('jwt', {session:false}), (req,res) => {
+router.post('/like/:id',passport.authenticate('jwt', {session:false}), (req,res) => {
     Profile.findOne({ user: req.user.id})
         .then(profile => {
             Post.findById(req.params.id)
@@ -99,6 +99,33 @@ router.post('like/:id',passport.authenticate('jwt', {session:false}), (req,res) 
                     }
 
                     post.likes.unshift({ user: req.user.id });
+                    post.save().then(post => res.json(post));
+                })
+                .catch(err => res.status(404).json({ postnotfound: 'No post found'}));
+        })
+
+});
+
+
+//route POST api/posts/unlike/:id
+// desc unlike post
+// access private
+router.post('/unlike/:id',passport.authenticate('jwt', {session:false}), (req,res) => {
+    Profile.findOne({ user: req.user.id})
+        .then(profile => {
+            Post.findById(req.params.id)
+                .then(post => {
+                    if(post.likes.filter(like => like.user.toString() === req.user.id)
+                    .length === 0) {
+                        return releaseEvents.status(400).json({notliked: 'You have not liked this post!'});
+                    }
+                    // get remove index 
+                    const  removeIndex = post.likes
+                        .map(item => item.user.toString())
+                        .indexOf(req.user.id);
+                    // remove from array 
+                    post.likes.splice(removeIndex, 1);
+                    
                     post.save().then(post => res.json(post));
                 })
                 .catch(err => res.status(404).json({ postnotfound: 'No post found'}));
