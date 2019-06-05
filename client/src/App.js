1bdd38243440cbed5f7a12991fc153ff9cab39ab
derrick-long <long.derrick8@gmail.com>
2019-06-05 13:36:39 -0400

    functioning login form, auth token

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import{ BrowserRouter as Router, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import jwt_decode from 'jwt-decode';
+import setAuthToken from './utils/setAuthToken';
+import { setCurrentUser } from './actions/authActions';
 import store from './store';
 
 import Navbar from './components/layout/Navbar';
@@ -12,7 +15,15 @@ import Login from './components/auth/Login';
 
 import './App.css';
 
-
+// check for token 
+if(localStorage.jwtToken){
+  // set auth token header
+  setAuthToken(localStorage.jwtToken);
+  //decode 
+  const decoded = jwt_decode(localStorage.jwtToken);
+  // set user/authenticate
+  store.dispatch(setCurrentUser(decoded));
+}
 
 class App extends Component {
   render() {
