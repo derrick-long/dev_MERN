e53dd14f4393131a24d570824af9fd112f2e4d43
derrick-long <long.derrick8@gmail.com>
2019-06-05 23:16:14 -0400

    add logout function

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,8 +1,8 @@
 import { GET_ERRORS } from './types';
-import setAuthToken from '../utils/setAuthToken';
 import jwt_decode from 'jwt-decode';
 import axios from 'axios';
 import { SET_CURRENT_USER } from './types';
+import setAuthToken from '../utils/setAuthToken';
 
 //Register User 
 export const registeruser = (userData, history)=> dispatch => {
@@ -44,5 +44,15 @@ export const setCurrentUser = (decoded) => {
     return {
         type: SET_CURRENT_USER,
         payload: decoded
-    }
-}
+    };
+};
+
+//log user out 
+export const logoutUser = () => dispatch => {
+    // Remove token from localStorage
+    localStorage.removeItem('jwtToken');
+    // Remove auth header for future requests
+    setAuthToken(false);
+    // Set current user to {} which will set isAuthenticated to false
+    dispatch(setCurrentUser({}));
+  };
